f7b4add1b035cc3c9d4d5f5fccc47367a55835d7
RuwanG-APP <hngrruwan@gmail.com>
2026-04-11 11:41:20 +0530

    Added firebase configuration details

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,25 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore } from "firebase/firestore"; // initializeFirestore ඇඩ් කළා
 
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "ඔයාගේ-API-KEY-එක",
-  authDomain: "ඔයාගේ-AUTH-DOMAIN-එක",
-  projectId: "ඔයාගේ-PROJECT-ID-එක",
-  storageBucket: "ඔයාගේ-STORAGE-BUCKET-එක",
-  messagingSenderId: "ඔයාගේ-SENDER-ID-එක",
-  appId: "ඔයාගේ-APP-ID-එක"
+  apiKey: "AIzaSyB2FOXA42Df70SE59MZgVEAV16eipXKnik",
+  authDomain: "my-bookshop-130c8.firebaseapp.com",
+  projectId: "my-bookshop-130c8",
+  storageBucket: "my-bookshop-130c8.firebasestorage.app",
+  messagingSenderId: "887183389723",
+  appId: "1:887183389723:web:e8b268bf13529f7f1a74be",
+  measurementId: "G-WPDTFCZ92H"
 };
 
-// 👇 Firebase App එක දැනටමත් තිබේ නම් එය පාවිච්චි කිරීමටත්, නැතිනම් අලුතින් සෑදීමටත් මෙය උපකාරී වේ.
+
+
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-const db = getFirestore(app);
+// 👇 මෙන්න මේ පේළිය පාවිච්චි කරන්න (සාමාන්‍ය getFirestore වෙනුවට)
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true, // මේකෙන් Connection එක ස්ථාවර කරනවා
+});
 
 export { db, app };
